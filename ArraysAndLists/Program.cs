adc28d5d218d9473566ec2704aa08928e86f38a2
alica kavuljakova <drobeek@gmail.com>
2023-05-03 18:42:00 -0700

    homework

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ArraysAndLists
 {
@@ -8,12 +10,35 @@ namespace ArraysAndLists
         {
             //TODO:
 
-            // Create an int Array and populate numbers 1-10
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Create an int Array and populate numbers 1-10
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            // Create two Lists of type int.
+            List<int> evens = new List<int> (); //* Name one List "evens"
+            List<int> odds = new List<int>(); ;// * Name the other List "odds"
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    evens.Add(numbers[i]);
+                }
+                else
+                {
+                    odds.Add(numbers[i]);
+                }
+            }
+            Console.WriteLine("Even numbers are :");
+            foreach (int num in evens)
+            {
+                Console.WriteLine(num);
+            }
+            Console.WriteLine("Odd numbers are: ");
+            foreach (int oddNum in odds)
+            {
+                Console.WriteLine(oddNum);
+            }
+        }
+    }
+}
 
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
@@ -27,6 +52,4 @@ namespace ArraysAndLists
              *
              * Try to be creative in your display
              */
-        }
-    }
-}
+   
